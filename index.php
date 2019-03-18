d3da4e582ed0493fdac3acc13c57da79b073b95e
fortunaign <43297312+fortunaign@users.noreply.github.com>
2019-03-18 15:59:32 -0400

    inicio

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Multimedia | Inicio</title>
+</head>
+<body>
+    <div id="container">
+        <div id="head">
+            <h1>Home page</h1>
+        </div>
+        <div id="menu">
+            <a href="">Entrar</a> | <a href="">Contenido</a>
+        </div>
+        <div id="cont">
+        
+        </div>
+        <div id="footer"> 
+            &copy;
+        </div>
+    </div>
+    
+</body>
+    <link rel="stylesheet" href="css/style.css">
+</html>
